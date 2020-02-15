5e915b3fd42cfe93bed5fb4e4f1b59c2dfb69c31
Park Ju Hyung <qkrwngud825@gmail.com>
2020-02-16 02:45:58 +0900

    surya: props: Relax timers for automatic refresh rate switching
    Due to visible gamma shifts on our panels, we should make the switchings
    to happen conservatively.
    
    With idle timer >= touch timer, idle timer now always takes precedence,
    but we're leaving touch timer property to immediately switch back to
    120 Hz on touch input events.
    
    Display power timer is left untouched as it's responsible for display
    mode changes such as Doze.
    
    Change-Id: If8b1e00a2d2213c4d2288ab28fad4ba0826a36e8
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- device.mk
+++ device.mk
@@ -158,8 +158,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Refresh Rate
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.set_idle_timer_ms=250 \
-    ro.surface_flinger.set_touch_timer_ms=300 \
+    ro.surface_flinger.set_idle_timer_ms=4000 \
+    ro.surface_flinger.set_touch_timer_ms=4000 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
