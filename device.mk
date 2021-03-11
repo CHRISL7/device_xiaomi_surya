4f08192b2706f448e2a21c5f5b1b3c3881013c9f
Jyotiraditya <jyotiraditya@aospa.co>
2021-03-11 09:35:28 +0100

    surya: props: Enable config_avoidGfxAccel
    - This flag turns off GPU/CPU intensive UI effects and
      reduces the RAM footprint.
    
    - Using this is a better compromise than the 'low_ram' flag
      as this does not disable features in the rom but still
      reduces RAM usage and gives better smoothness in the UI.
    
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>
    Signed-off-by: AkaruiAikara <vall@aikara.me>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- device.mk
+++ device.mk
@@ -134,6 +134,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_VENDOR_PROPERTIES += \
     debug.sf.latch_unsignaled=1 \
+    ro.config.avoid_gfx_accel=true \
     debug.sf.disable_client_composition_cache=1 \
     persist.sys.sf.native_mode=0 \
     persist.sys.sf.color_mode=0 \
