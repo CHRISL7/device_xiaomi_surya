bc91f654c6b545d22491e2a11d7ea15c86c8eca9
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-12-04 14:19:36 -0400

    surya: parts: Adapt Haptic Settings for A12 Style
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Change-Id: If11b41137fc48ab7074d9beb4c39fe8b1eb2c8f5

--- parts/src/org/lineageos/settings/haptic/HapticLevelActivity.java
+++ parts/src/org/lineageos/settings/haptic/HapticLevelActivity.java
@@ -17,9 +17,11 @@
 package org.lineageos.settings.haptic;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 
-public class HapticLevelActivity extends PreferenceActivity {
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class HapticLevelActivity extends CollapsingToolbarBaseActivity {
 
     private final String TAG_HAPTIC = "haptic_level";
 
@@ -27,7 +29,7 @@ public class HapticLevelActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(android.R.id.content,
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
                 new HapticLevelFragment(), TAG_HAPTIC)
                 .commit();
     }
--- parts/src/org/lineageos/settings/haptic/HapticLevelFragment.java
+++ parts/src/org/lineageos/settings/haptic/HapticLevelFragment.java
@@ -16,8 +16,9 @@
 
 package org.lineageos.settings.haptic;
 
-import android.app.ActionBar;
+import android.content.Context;
 import android.os.Bundle;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,9 +37,7 @@ public class HapticLevelFragment extends PreferenceFragment implements OnPrefere
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.haptic_settings);
 
-        final ActionBar actionBar = getActivity().getActionBar();
-        actionBar.setTitle(R.string.haptic_title);
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        getActivity().setTitle(R.string.haptic_title);
 
         final SeekBarPreference mHapticLevel = (SeekBarPreference) findPreference(HapticUtils.PREF_LEVEL);
         if (FileUtils.fileExists(HapticUtils.PATH_LEVEL)) {
