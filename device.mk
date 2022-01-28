d1ae650da1f7f7b0bbd31ff3b6ca274df86c32db
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-01-28 02:08:04 +0000

    surya: refreshrate: Return to Dynamic Refresh Rate
    [1] - Set new overlays setup
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -9,7 +9,7 @@ $(call inherit-product, firmware/xiaomi/surya/Android.mk)
 
 # RefreshRate Settings
 $(call inherit-product, device/xiaomi/surya/refreshrate.mk)
-USE_DYNAMIC_REFRESH_RATE := false
+USE_DYNAMIC_REFRESH_RATE := true
 
 # Device configs makefiles
 $(call inherit-product, device/xiaomi/surya/configs/device/hidl_vendor.mk)
