7bcb8ff0708e394a8f6fcd4bf2149a80fb39080d
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-03-04 13:24:22 +0000

    surya: hidl: Build bluetooth@1.0 vendor variant
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- configs/device/hidl_vendor.mk
+++ configs/device/hidl_vendor.mk
@@ -6,6 +6,7 @@
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
