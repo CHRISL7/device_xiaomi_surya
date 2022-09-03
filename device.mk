e142af716b759e4c0da5cd6c2b1e9a1c555326c8
Richard Raya <rdxzv.dev@gmail.com>
2022-09-03 14:25:44 -0300

    surya: Remove ParanoidDoze

--- device.mk
+++ device.mk
@@ -308,14 +308,6 @@ PRODUCT_PACKAGES += \
     SuryaFrameworks \
     SuryaWifiOverlay
 
-# ParanoidDoze
-PRODUCT_PACKAGES += \
-    ParanoidDoze
-
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.sensor.proximity=true \
-    ro.sensor.pickup=xiaomi.sensor.pickup
-
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
