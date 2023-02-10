fc15d8f12246b5ee969846a66ff64f4ffeab247b
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-02-10 11:41:32 -0800

    surya: sepolicy: Import sepolicy rules from J20C-sepolicy
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

---
+++ sepolicy/surya-sepolicy.mk
@@ -0,0 +1,24 @@
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/xiaomi/surya/sepolicy/audio/vendor \
+    device/xiaomi/surya/sepolicy/battery/vendor \
+    device/xiaomi/surya/sepolicy/batterysecret/vendor \
+    device/xiaomi/surya/sepolicy/common/vendor \
+    device/xiaomi/surya/sepolicy/power/vendor \
+    device/xiaomi/surya/sepolicy/camera/vendor \
+    device/xiaomi/surya/sepolicy/ir/vendor \
+    device/xiaomi/surya/sepolicy/fingerprint/vendor \
+    device/xiaomi/surya/sepolicy/leds/vendor \
+    device/xiaomi/surya/sepolicy/nfc/vendor \
+    device/xiaomi/surya/sepolicy/perf/vendor \
+    device/xiaomi/surya/sepolicy/qcom-extra/vendor \
+    device/xiaomi/surya/sepolicy/telephony/vendor \
+    device/xiaomi/surya/sepolicy/thermal/vendor \
+    device/xiaomi/surya/sepolicy/usb/vendor
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/xiaomi/surya/sepolicy/audio/private \
+    device/xiaomi/surya/sepolicy/camera/private \
+    device/xiaomi/surya/sepolicy/common/private
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    device/xiaomi/surya/sepolicy/camera/public
