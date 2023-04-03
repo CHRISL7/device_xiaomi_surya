a2ac37680ff197a431ee6954b621c1fdb321e592
KUIpru <rakeshtiwari349@gmail.com>
2023-04-03 10:14:38 +0000

    Revert "surya: Drop livedisplay"
    This reverts commit d214e062f244d9ee41daa1258f9aa737d9a7cddf.

--- device.mk
+++ device.mk
@@ -265,6 +265,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.1-service.surya
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
