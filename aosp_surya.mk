e461a2bf5b2a810c2e37c203f51ab46d8a9daf46
KUIpru <rakeshtiwari349@gmail.com>
2023-04-05 05:35:51 +0000

    surya: ActualReality OS bringup

--- aosp_surya.mk
+++ aosp_surya.mk
@@ -11,14 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common ActualRealityOS stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-# Target
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
+AROS_MAINTAINER := KUI
 
-# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
